e25a293306b59fafd72615b1dd4c649333a00a10
Kaweeshwara <githubkawe@gmail.com>
2025-01-23 14:39:10 +0530

    Added Turn Based Functionality to the Game

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-const rollButton = document.querySelector(".btn-roll");
+let currentPlayer = 1;
+let player1Roll = 0;
+let player2Roll = 0;
 
-rollButton.addEventListener("click", () => {
-  const randomNumber1 = Math.floor(Math.random() * 6) + 1;
-  const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+const heading = document.querySelector(".header");
+const diceButton = document.querySelector(".btn-roll");
+const player1Img = document.querySelector(".player1-img");
+const player2Img = document.querySelector(".player2-img");
+const player1Title = document.querySelector(".player1-title");
+const player2Title = document.querySelector(".player2-title");
 
-  const player1Img = document.querySelector(".player1-img");
-  player1Img.setAttribute("src", `images/dice${randomNumber1}.png`);
-
-  const player2Img = document.querySelector(".player2-img");
-  player2Img.setAttribute("src", `images/dice${randomNumber2}.png`);
-
-  const winnerDeclearation = document.querySelector(".header");
-
-  if (randomNumber1 > randomNumber2) {
-    winnerDeclearation.innerHTML = `Player 1 won &#x1F3C6;`;
-  } else if (randomNumber1 === randomNumber2) {
-    winnerDeclearation.innerHTML = `Draw Both Win &#x1F3C6;`;
+diceButton.addEventListener("click", () => {
+  if (currentPlayer === 1) {
+    player1Title.classList.add("player1Highlight");
+    const random1 = Math.floor(Math.random() * 6) + 1;
+    player1Img.setAttribute("src", `images/dice${random1}.png`);
+    heading.innerHTML = `Player 2 Turn`;
+    player1Roll = random1;
+    currentPlayer = 0;
   } else {
-    winnerDeclearation.innerHTML = `Player 2 won &#x1F3C6;`;
+    player1Title.classList.remove("player1Highlight");
+    player2Title.classList.add("player2Highlight");
+    const random2 = Math.floor(Math.random() * 6) + 1;
+    player2Img.setAttribute("src", `images/dice${random2}.png`);
+
+    player2Roll = random2;
+
+    if (player1Roll > player2Roll) {
+      heading.innerHTML = `Player 1 won &#x1F3C6; `;
+    } else if (player1Roll < player2Roll) {
+      heading.innerHTML = `Player 2 won &#x1F3C6; `;
+    } else {
+      heading.innerHTML = `Draw Both Win  &#x1F3C6; `;
+    }
+    currentPlayer = 1;
+    setTimeout(() => {
+      heading.innerHTML = "Player 1's Turn to Roll! 🎲";
+      player2Title.classList.remove("player2Highlight");
+      player1Img.setAttribute("src", `images/dice1.png`);
+      player2Img.setAttribute("src", `images/dice1.png`);
+      player1Roll = 0;
+      player2Roll = 0;
+    }, 2000);
   }
 });
